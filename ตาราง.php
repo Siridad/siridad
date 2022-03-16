31fb397b1211d94a644566f09a5722f22fda90d3
Siridad <101719139+Siridad@users.noreply.github.com>
2022-03-16 22:43:59 +0700

    Create ตาราง.php

---
+++ ตาราง.php
@@ -0,0 +1,138 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>ปฎิพัทธ์ อัศวเสนา</td>
+        <td>อัส</td>
+        <td><img src="https://asianwiki.com/images/e/e8/Song_Joong-Ki-p002.jpeg" width="130px"></td>
+        <td><a href="https://twitter.com/asuka111">twitter</a></td>
+      </tr>  
+    </table>
+  </body> 
+</html>
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>ณัชนน เลียงมั้น</td>
+        <td>พีพี</td>
+        <td><img src="https://asianwiki.com/images/e/e8/Song_Joong-Ki-p002.jpeg" width="130px"></td>
+        <td><a href="-">twitter</a></td>
+      </tr>  
+    </table>
+  </body> 
+</html>
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>ปัณณ์ เสมาทอง</td>
+        <td>กีกี้</td>
+        <td><img src="https://asianwiki.com/images/e/e8/Song_Joong-Ki-p002.jpeg" width="130px"></td>
+        <td><a href="-">twitter</a></td>
+      </tr>  
+    </table>
+  </body> 
+</html>
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>ทนาพาวิน แสงยส</td>
+        <td>วิน</td>
+        <td><img src="https://scontent.fbkk8-4.fna.fbcdn.net/v/t39.30808-6/270842127_1339635939820327_5102343321798523151_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeEGrS1qZAiG1jBQwJJR9l3jU_sBD0BmBYZT-wEPQGYFhrd-CGX_AguQT55zLrSje2kpwqTYj0qST2b__4R3Orfm&_nc_ohc=YajqwhkPrigAX-Trzkg&_nc_ht=scontent.fbkk8-4.fna&oh=00_AT8cI1o0e_AGpRVaj2NXpNuuP4Fc6jhtlz3xwLBrsGWkoA&oe=6237B511" width="130px"></td>
+        <td><a href="https://twitter.com/mare01night?s=21">twitter</a></td>
+      </tr>  
+    </table>
+  </body> 
+</html>
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>กิสนะชัย สุวันปาวัด</td>
+        <td>น็อต</td>
+        <td><img src="https://asianwiki.com/images/e/e8/Song_Joong-Ki-p002.jpeg" width="130px"></td>
+        <td><a href="https://twitter.com/mare01night?s=21">twitter</a></td>
+      </tr>  
+    </table>
+  </body> 
+</html>
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td>ขม</td>
+        <td><img src="https://asianwiki.com/images/e/e8/Song_Joong-Ki-p002.jpeg" width="130px"></td>
+        <td><a href="https://twitter.com/sjk_kith?lang=en">twitter</a></td>
+      </tr>  
+    </table>
+  </body> 
+</html>
